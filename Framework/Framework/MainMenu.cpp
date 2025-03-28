914e725a456947a35197d98207ed673d96216500
JPM-enti <joel.parra@enti.cat>
2025-03-28 13:59:57 +0100

    Menu & ranking update
    Titulos bonitos :3

--- Framework/Framework/MainMenu.cpp
+++ Framework/Framework/MainMenu.cpp
@@ -1,12 +1,13 @@
 #include "MenuScene.h"
+#include "ConsoleControl.h"
 
 void MenuScene::Update()
 {
-	if (GetAsyncKeyState('1') != 0) {
+	if (GetAsyncKeyState('2') != 0) {
 		nextScene = "Ranking"; //Selects the next scene 
 		finished = true; //Indicates that this scene is finished
 	}
-	else if (GetAsyncKeyState('2') != 0) {
+	else if (GetAsyncKeyState('1') != 0) {
 		nextScene = "Gameplay";
 		finished = true;
 
@@ -19,5 +20,18 @@ void MenuScene::Update()
 
 void MenuScene::Render()
 {
-	std::cout << "Press 1 for Ranking and 2 for Gameplay";
+	ConsoleSetColor(MAGENTA, BLACK);
+	std::cout << "__________                        __   ________          __   " << std::endl;
+	std::cout << "\\______   \\_______   ____ _____  |  | _\\_____  \\  __ ___/  |_ " << std::endl;
+	std::cout << " |    |  _/\\_  __ \\_/ __ \\\\__  \\ |  |/ //   |   \\|  |  \\   __\\" << std::endl;
+	std::cout << " |    |   \\ |  | \\/\\  ___/ / __ \\|    </    |    \\  |  /|  |  " << std::endl;
+	std::cout << " |______  / |__|    \\___  >____  /__|_ \\_______  /____/ |__|  " << std::endl;
+	std::cout << "        \\/              \\/     \\/     \\/       \\/             " << std::endl;
+
+	ConsoleSetColor(BLUE, BLACK);
+	std::cout << "\n\n\n" << "1- Gameplay\n" << "2- Ranking\n";
+
+	ConsoleSetColor(RED, BLACK);
+	std::cout << "3- Exit";
+	ConsoleSetColor(WHITE, BLACK);
 }
--- Framework/Framework/RankingScene.cpp
+++ Framework/Framework/RankingScene.cpp
@@ -1,4 +1,5 @@
 #include "RankingScene.h"
+#include "ConsoleControl.h"
 
 void RankingScene::Update()
 {
@@ -9,5 +10,16 @@ void RankingScene::Update()
 
 void RankingScene::Render()
 {
-	std::cout << "Press R to go back to the menu;";
+	ConsoleSetColor(MAGENTA, BLACK);
+	std::cout << "__________                __    __                " << std::endl;
+	std::cout << "\\______   \\_____    ____ |  | _|__| ____    ____  " << std::endl;
+	std::cout << " |       _/\\__  \\  /    \\|  |/ /  |/    \\  / ___\\ " << std::endl;
+	std::cout << " |    |   \\ / __ \\|   |  \\    <|  |   |  \\/ /_/  >" << std::endl;
+	std::cout << " |____|_  /(____  /___|  /__|_ \\__|___|  /\\___  / " << std::endl;
+	std::cout << "        \\/      \\/     \\/     \\/       \\//_____/  " << std::endl;
+
+	ConsoleSetColor(RED, BLACK);
+	std::cout << "\nPress R to go back to the menu;";
+	ConsoleSetColor(WHITE, BLACK);
+
 }
